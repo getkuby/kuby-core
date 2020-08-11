c78ed4011d0ccbea80a63976890ae347201b5e5a
Simon Træls Ravn <cs2@cs2.dk>
2020-08-11 22:56:26 +0200

    Chore. Clearer test using better RSpec syntax.

--- spec/docker/timestamp_tag_spec.rb
+++ spec/docker/timestamp_tag_spec.rb
@@ -1,9 +1,11 @@
-RSpec.describe Kuby::Docker::TimestampTag, ".try_parse" do
-  context "with valid timestamp string" do
-    it "returns a new timestamp tag" do
-      tag = Kuby::Docker::TimestampTag.try_parse("20200810165134")
+require "spec_helper"
 
-      expect(tag).to be_kind_of(Kuby::Docker::TimestampTag)
+describe Kuby::Docker::TimestampTag do
+  context ".try_parse" do
+    it "returns a new timestamp tag" do
+      tag = described_class.try_parse("20200810165134")
+
+      expect(tag).to be_kind_of(described_class)
     end
   end
 end
