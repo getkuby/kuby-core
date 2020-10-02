5d9bace4ab209fffed14b0fb3f7beb964bd34690
Cameron Dutro <camertron@gmail.com>
2020-10-02 09:45:02 -0700

    Huh, I guess srb generics really aren't ready

--- lib/kuby/trailing_hash.rb
+++ lib/kuby/trailing_hash.rb
@@ -2,13 +2,6 @@
 
 module Kuby
   class TrailingHash < Hash
-    extend T::Sig
-    extend T::Generic
-
-    K = type_member
-    V = type_member
-
-    sig { params(block: T.nilable(T.proc.params(key: K, value: V).void)).void }
     def each(&block)
       return to_enum(T.must(__method__)) unless block_given?
 
--- spec/trailing_hash_spec.rb
+++ spec/trailing_hash_spec.rb
@@ -1,4 +1,5 @@
 # typed: false
+
 require 'spec_helper'
 
 describe Kuby::TrailingHash do
