c1dc3dc89b4a812dc38fcffd303295ef988ef928
Cameron Dutro <camertron@gmail.com>
2020-10-22 10:21:36 -0700

    Fold; copy kube config

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+echo travis_fold:start:kubeadm
 sudo apt-get update
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -8,7 +9,10 @@ EOF
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo kubeadm init
-kubectl taint nodes --all node-role.kubernetes.io/master-
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+echo travis_fold:end:kubeadm
+
+kubectl get ns
