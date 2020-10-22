f94b67abbb4367fca0c4fc615fb27ee32df11424
Cameron Dutro <camertron@gmail.com>
2020-10-22 10:15:11 -0700

    Enable/use sudo

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-apt-get update
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-kubeadm init
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo kubeadm init
 kubectl taint nodes --all node-role.kubernetes.io/master-
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
