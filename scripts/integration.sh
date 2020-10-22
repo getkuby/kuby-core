eb1a71d2448189b9c86f9e755cfe969d3b9c2368
Cameron Dutro <camertron@gmail.com>
2020-10-22 11:48:37 -0700

    Attempt to generate rails app

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,18 +1,25 @@
 #! /bin/bash
 
-echo travis_fold:start:kubeadm
-sudo apt-get update
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+function setup_cluster() {
+  echo travis_fold:start:setup_cluster
+
+  # install kubeadm and use it to set up the cluster
+  sudo apt-get update
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo kubeadm init
-mkdir -p ~/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
-sudo chown $(id -u):$(id -g) ~/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/master-
-echo travis_fold:end:kubeadm
+  sudo apt-get update
+  sudo apt-get install -y kubelet kubeadm kubectl
+  sudo kubeadm init
 
-kubectl get ns
+  # copy kubeconfig to default location so kubectl works
+  mkdir -p ~/.kube
+  sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+  sudo chown $(id -u):$(id -g) ~/.kube/config
+
+  # allow pods to be scheduled on the master node
+  kubectl taint nodes --all node-role.kubernetes.io/master-
+
+  echo travis_fold:end:setup_cluster
+}
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,5 +5,16 @@ if [[ "$STAGE" == 'test' ]]; then
 elif [[ "$STAGE" == 'typecheck' ]]; then
   srb tc
 elif [[ "$STAGE" == "integration" ]]; then
-  ./scripts/integration.sh
+  source ./scripts/integration.sh
+  setup_cluster
+
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt update && sudo apt-get install libmysqlclient-dev nodejs yarn
+  gem install rails -v 6.0.3.4
+  rails _6.0.3.4_ new kubyapp -d mysql
+  cd kubyapp
+  printf "\ngem 'kuby-core', path: '../'\n" >> Gemfile
+  bundle exec rails g kuby
+  bundle exec kuby -e production build
 fi
