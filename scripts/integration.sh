8ca89870076cd1d95285f6f8ce45017d598c0ffb
Cameron Dutro <camertron@gmail.com>
2020-10-23 10:55:10 -0700

    Refactor; print GLI debug output

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -3,6 +3,8 @@
 function setup_cluster() {
   echo travis_fold:start:setup_cluster
 
+  set -o xtrace
+
   # install kubeadm and use it to set up the cluster
   sudo apt-get update
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -20,6 +22,24 @@ EOF
 
   # allow pods to be scheduled on the master node
   kubectl taint nodes --all node-role.kubernetes.io/master-
+  set +o xtrace
 
   echo travis_fold:end:setup_cluster
+}
+
+function setup_node() {
+  set +o xtrace
+  source ~/.nvm/nvm.sh
+
+  echo travis_fold:start:setup_node
+
+  set -o xtrace
+  nvm install 15.0.1
+  nvm use 15.0.1
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt-get update && sudo apt-get install -y yarn
+  set +o xtrace
+
+  echo travis_fold:end:setup_node
 }
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,14 +10,8 @@ elif [[ "$STAGE" == "integration" ]]; then
 
   source ./scripts/integration.sh
   setup_cluster
+  setup_node
 
-  source ~/.nvm/nvm.sh
-  set -o xtrace
-  nvm install 15.0.1
-  nvm use 15.0.1
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt-get update && sudo apt-get install -y libmysqlclient-dev yarn
   gem install rails -v 6.0.3.4
   cd ..
   rails _6.0.3.4_ new kubyapp -d mysql
@@ -29,8 +23,8 @@ elif [[ "$STAGE" == "integration" ]]; then
   printf "Kuby.environment.docker.image_url('kubyapp')\n" >> kuby.rb
   printf "Kuby.environment.kubernetes.plugin(:rails_app).tls_enabled(false)\n" >> kuby.rb
 
-  bundle exec kuby -e production build
-  bundle exec kuby -e production deploy
+  GLI_DEBUG=true bundle exec kuby -e production build
+  GLI_DEBUG=true bundle exec kuby -e production deploy
 
   curl kubyapp-web:8080
 fi
