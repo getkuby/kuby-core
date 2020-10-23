31ce7a26a488dee5c9167e733980d667f9753b04
Cameron Dutro <camertron@gmail.com>
2020-10-23 10:57:23 -0700

    Add more folds

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,6 +12,7 @@ elif [[ "$STAGE" == "integration" ]]; then
   setup_cluster
   setup_node
 
+  echo travis_fold:start:generate_app
   gem install rails -v 6.0.3.4
   cd ..
   rails _6.0.3.4_ new kubyapp -d mysql
@@ -22,9 +23,15 @@ elif [[ "$STAGE" == "integration" ]]; then
   printf "Kuby.environment.docker.insert(:vendor, before: :bundler_phase) { |dockerfile| dockerfile.copy('vendor', 'vendor') }\n" >> kuby.rb
   printf "Kuby.environment.docker.image_url('kubyapp')\n" >> kuby.rb
   printf "Kuby.environment.kubernetes.plugin(:rails_app).tls_enabled(false)\n" >> kuby.rb
+  echo travis_fold:end:generate_app
 
+  echo travis_fold:start:build
   GLI_DEBUG=true bundle exec kuby -e production build
+  echo travis_fold:end:build
+
+  echo travis_fold:start:deploy
   GLI_DEBUG=true bundle exec kuby -e production deploy
+  echo travis_fold:end:deploy
 
   curl kubyapp-web:8080
 fi
