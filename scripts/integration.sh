1b8e4241ecc29101fdcde04c039f9b6e7ccf503d
Cameron Dutro <camertron@gmail.com>
2021-05-03 15:01:26 -0700

    Fix load path issue

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -106,7 +106,6 @@ class PrebundlerPhase < Kuby::Docker::BundlerPhase
     dockerfile.env('PREBUNDLER_SECRET_ACCESS_KEY', app_creds[:PREBUNDLER_SECRET_ACCESS_KEY])
 
     dockerfile.copy('.prebundle_config', '.')
-    dockerfile.run(%w(pwd))
     # dockerfile.run('gem', 'install', 'prebundler', '-v', "'< 1'")
 
     super
