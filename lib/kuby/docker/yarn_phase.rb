9954816e6527e1dd878b1cb61d0ae50eb8792491
Cameron Dutro <camertron@gmail.com>
2021-05-04 10:51:33 -0700

    Woops

--- lib/kuby/docker/yarn_phase.rb
+++ lib/kuby/docker/yarn_phase.rb
@@ -8,10 +8,10 @@ module Kuby
       sig { params(dockerfile: Dockerfile).void }
       def apply_to(dockerfile)
         dockerfile.copy('package.json', '.')
-        # use character classes as a hack to only copy the files if they exist
-        dockerfile.copy('yarn.loc[k]', '.')
-        dockerfile.copy('.npmr[c]', '.')
-        dockerfile.copy('.yarnr[c]', '.')
+        # use wildcards as a hack to only copy the files if they exist
+        dockerfile.copy('yarn.lock*', '.')
+        dockerfile.copy('.npmrc*', '.')
+        dockerfile.copy('.yarnrc*', '.')
         dockerfile.run('yarn', 'install')
       end
     end
