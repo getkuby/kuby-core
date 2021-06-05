ca83a8211af45c5cf5cbc5af900ae6b468349e63
Cameron Dutro <camertron@gmail.com>
2021-06-05 14:10:11 -0700

    Fix bare metal provider

--- lib/kuby/kubernetes/bare_metal_provider.rb
+++ lib/kuby/kubernetes/bare_metal_provider.rb
@@ -19,7 +19,7 @@ module Kuby
       attr_reader :config
 
       sig { void }
-      def initialize
+      def initialize(environment)
         @config = T.let(Config.new, Config)
         super
       end
