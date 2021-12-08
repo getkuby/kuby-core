16a3c4691535c0d404357b37b34c7bb84c24f3fd
Cameron Dutro <camertron@gmail.com>
2021-12-08 15:44:41 -0800

    Try this?

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -71,7 +71,7 @@ end
 EOF
 prebundle install --jobs 2 --retry 3
 yarn install
-bundle exec rails app:update:bin
+bundle exec bin/rails app:update:bin
 bundle exec bin/rails g kuby
 cat <<'EOF' > kuby.rb
 class PrebundlerPhase < Kuby::Docker::BundlerPhase
