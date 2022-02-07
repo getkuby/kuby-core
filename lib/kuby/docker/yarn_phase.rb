111ad834702ad1f57061d8d2bc2c21f9f9697d30
Cameron Dutro <camertron@gmail.com>
2022-02-07 14:52:28 -0800

    Fix yarn phase so files are copied according to Docker's path rules

--- lib/kuby/docker/yarn_phase.rb
+++ lib/kuby/docker/yarn_phase.rb
@@ -8,8 +8,14 @@ module Kuby
       sig { params(dockerfile: Dockerfile).void }
       def apply_to(dockerfile)
         host_path = environment.docker.app_root_path
-        container_path = Pathname(dockerfile.current_workdir).join(
-          environment.docker.app_root_path
+
+        # if more than one file is passed to the COPY directive, the destination must be
+        # a directory and must end with a slash
+        container_path = ensure_trailing_delimiter(
+          File.join(
+            dockerfile.current_workdir,
+            environment.docker.app_root_path
+          )
         )
 
         # use brackets as a hack to only copy the files if they exist
@@ -19,6 +25,12 @@ module Kuby
         )
 
         dockerfile.run('yarn', 'install', '--cwd', container_path)
+      end
+
+      private
+
+      def ensure_trailing_delimiter(path)
+        path.end_with?(File::SEPARATOR) ? path : File.join(path, '')
       end
     end
   end
