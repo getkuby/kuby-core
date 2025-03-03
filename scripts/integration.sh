cb91fecdd46c8268bea3f70f80c20de3d223adb0
Cameron Dutro <camertron@gmail.com>
2025-03-03 10:02:30 -0800

    Run prebundler first

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -2,52 +2,11 @@
 
 set -e
 
-# clone rails app
-gem install prebundler -v '< 1'
-git clone https://github.com/getkuby/kuby_test.git
-cp -r kuby-core/ kuby_test/vendor/
-cd kuby_test
-
-# remove sorbet annotations
-gem install curdle
-curdle $(find vendor/kuby-core/lib -name '*.rb') > /dev/null
-
-# gems
-printf "\ngem 'kuby-core', path: 'vendor/kuby-core'\n" >> Gemfile
-printf "gem 'kuby-prebundler', '~> 0.1'\n" >> Gemfile
-printf "gem 'kuby-kind', '~> 0.2'\n" >> Gemfile
-printf "gem 'kuby-sidekiq', '~> 0.3'\n" >> Gemfile
-printf "gem 'sidekiq', '~> 6.5'\n" >> Gemfile
-
 # install ruby deps
-bundle lock
-
-cat <<'EOF' > .prebundle_config
-Prebundler.configure do |config|
-  config.storage_backend = Prebundler::S3Backend.new(
-    client: Aws::S3::Client.new(
-      region: 'default',
-      credentials: Aws::Credentials.new(
-        ENV['PREBUNDLER_ACCESS_KEY_ID'],
-        ENV['PREBUNDLER_SECRET_ACCESS_KEY']
-      ),
-      endpoint: 'https://us-east-1.linodeobjects.com',
-      http_continue_timeout: 0
-    ),
-    bucket: 'prebundler2',
-    region: 'us-east-1'
-  )
-end
-EOF
-
-prebundle install --jobs 2 --retry 3 --no-binstubs
+source scripts/prebundle.sh
 
 # javascript deps, cxx flags because node-sass is a special snowflake
 CXXFLAGS="--std=c++17" yarn install
-
-# bootstrap app for use with kuby
-bundle exec bin/rails g kuby
-prebundle install --jobs 2 --retry 3 --no-binstubs
 
 cat <<EOF > kuby.rb
 class VendorPhase < Kuby::Docker::Layer
